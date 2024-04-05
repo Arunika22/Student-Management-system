3d64b2bced066ebf738af5d96122f1ce51c7b5ae
Arunika <arunika2003@gmail.com>
2024-04-05 17:51:36 +0530

    Hod home update

--- dashboard/StaffViews.py
+++ dashboard/StaffViews.py
@@ -76,14 +76,7 @@ def staff_profile_update(request):
             messages.error(request, "Failed to Update Profile") 
             return redirect('staff_profile') 
 
-def staff_take_attendance(request): 
-    subjects = Subjects.objects.filter(staff_id=request.user.id) 
-    session_years = SessionYearModel.objects.all() 
-    context = { 
-        "subjects": subjects, 
-        "session_years": session_years 
-    } 
-    return render(request, "faculty page/attendance.html", context)
+
  
 def upload_notes(request):
     print("Inside upload_notes view function")  # Debugging statement
@@ -161,3 +154,190 @@ def edit_notes(request, note_id):
         'note_subject': note_subject
     }
     return render(request, 'faculty page/notes.html', context)
+def staff_take_attendance(request): 
+    subjects = Subjects.objects.filter(staff_id=request.user.id) 
+    session_years = SessionYearModel.objects.all() 
+    context = { 
+        "subjects": subjects, 
+        "session_years": session_years 
+    } 
+    return render(request, "faculty page/attendance.html", context) 
+@csrf_exempt
+def save_attendance_data(request): 
+    
+    # Get Values from Staf Take Attendance form via AJAX (JavaScript) 
+    # Use getlist to access HTML Array/List Input Data 
+    student_ids = request.POST.get("student_ids") 
+    subject_id = request.POST.get("subject_id") 
+    attendance_date = request.POST.get("attendance_date") 
+    session_year_id = request.POST.get("session_year_id") 
+  
+    subject_model = Subjects.objects.get(id=subject_id) 
+    session_year_model = SessionYearModel.objects.get(id=session_year_id) 
+  
+    json_student = json.loads(student_ids) 
+      
+    try: 
+        # First Attendance Data is Saved on Attendance Model 
+        attendance = Attendance(subject_id=subject_model, 
+                                attendance_date=attendance_date, 
+                                session_year_id=session_year_model) 
+        attendance.save() 
+  
+        for stud in json_student: 
+            # Attendance of Individual Student saved on AttendanceReport Model 
+            student = Students.objects.get(admin=stud['id']) 
+            attendance_report = AttendanceReport(student_id=student, 
+                                                 attendance_id=attendance, 
+                                                 status=stud['status']) 
+            attendance_report.save() 
+        return HttpResponse("OK") 
+    except: 
+        return HttpResponse("Error") 
+  
+  
+  
+  
+def staff_update_attendance(request): 
+    subjects = Subjects.objects.filter(staff_id=request.user.id) 
+    session_years = SessionYearModel.objects.all() 
+    context = { 
+        "subjects": subjects, 
+        "session_years": session_years 
+    } 
+    return render(request, "faculty page/attendance.html", context) 
+
+@csrf_exempt
+def get_students(request): 
+    
+    subject_id = request.POST.get("subject") 
+    session_year = request.POST.get("session_year") 
+  
+    # Students enroll to Course, Course has Subjects 
+    # Getting all data from subject model based on subject_id 
+    subject_model = Subjects.objects.get(id=subject_id) 
+  
+    session_model = SessionYearModel.objects.get(id=session_year) 
+  
+    students = Students.objects.filter(course_id=subject_model.course_id, 
+                                       session_year_id=session_model) 
+  
+    # Only Passing Student Id and Student Name Only 
+    list_data = [] 
+  
+    for student in students: 
+        data_small={"id":student.admin.id, 
+                    "name":student.admin.first_name+" "+student.admin.last_name} 
+        list_data.append(data_small) 
+  
+    return JsonResponse(json.dumps(list_data), content_type="application/json", safe=False) 
+  
+@csrf_exempt
+def save_attendance_data(request): 
+    
+    # Get Values from Staf Take Attendance form via AJAX (JavaScript) 
+    # Use getlist to access HTML Array/List Input Data 
+    student_ids = request.POST.get("student_ids") 
+    subject_id = request.POST.get("subject_id") 
+    attendance_date = request.POST.get("attendance_date") 
+    session_year_id = request.POST.get("session_year_id") 
+  
+    subject_model = Subjects.objects.get(id=subject_id) 
+    session_year_model = SessionYearModel.objects.get(id=session_year_id) 
+  
+    json_student = json.loads(student_ids) 
+      
+    try: 
+        # First Attendance Data is Saved on Attendance Model 
+        attendance = Attendance(subject_id=subject_model, 
+                                attendance_date=attendance_date, 
+                                session_year_id=session_year_model) 
+        attendance.save() 
+  
+        for stud in json_student: 
+            # Attendance of Individual Student saved on AttendanceReport Model 
+            student = Students.objects.get(admin=stud['id']) 
+            attendance_report = AttendanceReport(student_id=student, 
+                                                 attendance_id=attendance, 
+                                                 status=stud['status']) 
+            attendance_report.save() 
+        return HttpResponse("OK") 
+    except: 
+        return HttpResponse("Error") 
+  
+  
+@csrf_exempt
+def get_attendance_dates(request): 
+      
+  
+    # Getting Values from Ajax POST 'Fetch Student' 
+    subject_id = request.POST.get("subject") 
+    session_year = request.POST.get("session_year_id") 
+  
+    # Students enroll to Course, Course has Subjects 
+    # Getting all data from subject model based on subject_id 
+    subject_model = Subjects.objects.get(id=subject_id) 
+  
+    session_model = SessionYearModel.objects.get(id=session_year) 
+    attendance = Attendance.objects.filter(subject_id=subject_model, 
+                                           session_year_id=session_model) 
+  
+    # Only Passing Student Id and Student Name Only 
+    list_data = [] 
+  
+    for attendance_single in attendance: 
+        data_small={"id":attendance_single.id, 
+                    "attendance_date":str(attendance_single.attendance_date), 
+                    "session_year_id":attendance_single.session_year_id.id} 
+        list_data.append(data_small) 
+  
+    return JsonResponse(json.dumps(list_data), 
+                        content_type="application/json", safe=False) 
+  
+  
+@csrf_exempt
+def get_attendance_student(request): 
+    
+    # Getting Values from Ajax POST 'Fetch Student' 
+    attendance_date = request.POST.get('attendance_date') 
+    attendance = Attendance.objects.get(id=attendance_date) 
+  
+    attendance_data = AttendanceReport.objects.filter(attendance_id=attendance) 
+    # Only Passing Student Id and Student Name Only 
+    list_data = [] 
+  
+    for student in attendance_data: 
+        data_small={"id":student.student_id.admin.id, 
+                    "name":student.student_id.admin.first_name+" "+student.student_id.admin.last_name, "status":student.status} 
+        list_data.append(data_small) 
+  
+    return JsonResponse(json.dumps(list_data), 
+                        content_type="application/json", 
+                        safe=False) 
+  
+  
+@csrf_exempt
+def update_attendance_data(request): 
+    student_ids = request.POST.get("student_ids") 
+  
+    attendance_date = request.POST.get("attendance_date") 
+    attendance = Attendance.objects.get(id=attendance_date) 
+  
+    json_student = json.loads(student_ids) 
+  
+    try: 
+          
+        for stud in json_student: 
+            
+            # Attendance of Individual Student saved on AttendanceReport Model 
+            student = Students.objects.get(admin=stud['id']) 
+  
+            attendance_report = AttendanceReport.objects.get(student_id=student, 
+                                                             attendance_id=attendance) 
+            attendance_report.status=stud['status'] 
+  
+            attendance_report.save() 
+        return HttpResponse("OK") 
+    except: 
+        return HttpResponse("Error") 
+  
